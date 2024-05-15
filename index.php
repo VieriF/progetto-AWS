d1602ad17fd1295584b80fbe6e4b26a0ca7aec9c
VieriF <128815046+VieriF@users.noreply.github.com>
2024-05-15 19:01:04 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>EOTY Project</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+          <a class="navbar-brand " href="#">
+              <img src="../images/calendar_icon.png" alt="Icona del calendario" width="30" height="30">
+          </a>
+        <a class="navbar-brand" href="#">Shared Calendar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="login.html">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="register.html">Register</a>
+            </li>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="center-text">
+        <div class="text">
+          <h1 class="title">Cosa facciamo?</h1>
+          <p class="font-size-up">
+            In questo progetto abbiamo pensato di realizzare uno "shared calendar" tra utenti;
+            la sua funzionalità principale risiede nel condividere tra gli utilizzatori dell'applicazione di condividere tra loro dati e informazioni
+            basandosi su un database e un insieme di funzioni javascript e codice php
+            se si è pronti per provare il lavoro, fare Login o Registrarsi.
+          </p>
+        </div>
+        <div class="image"></div>
+      </div>
+    </div>
+    <div class="p-3 bg-secondary-subtle text-secondary-emphasis footer"> - Lavoro a carico di: Alessandro Tognella, Francesco Vieri, Nikolas Megna -  </div>
+</body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </html>
