70376cb75888dcdf4e149c6923bc953f6772df79
VieriF <128815046+VieriF@users.noreply.github.com>
2024-05-23 22:36:16 +0200

    Update login.php
    MAREMMA MAIALA FUNZIONA

--- login.php
+++ login.php
@@ -1,20 +1,15 @@
 <?php
 include "connect.php";
 
-if(isset($_POST['username']) && isset($_POST['password'])){
-    $username = $_POST['username'];
-    $password = md5($_POST['password']);
-	$sql = "SELECT * FROM utenti WHERE username = '$username' 
-			AND password = '$password'";
+if(isset($_POST['email']) && isset($_POST['password'])){
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+	$sql = "SELECT * FROM utenti WHERE username = '$email' AND password = '$password'";
 	$res = $conn -> query($sql);
 	if($res -> num_rows > 0){
-		header("Location: home.php");
+		header("Location: principale.php");
 	}else{
-		echo "<script>";
-        echo "var message = 'Utente non trovato';";
-        echo "alert(message);";
-        echo "window.location.href = 'login.php';";
-        echo "</script>";
+		header("Location: index.php");
 	}
 }
 ?>
